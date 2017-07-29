cbfa36ed3452859c3bc310952fe2d9a8aa4ecee6
Dmitrij Ivanenko <ivanenko.dmitrij@gmail.com>
2017-07-29 21:10:20 +0300

    fmt and comments changes

--- email.go
+++ email.go
@@ -8,7 +8,7 @@ func (g *Generator) Email() string {
 	return g.GenericGenerator("email")
 }
 
-// Address function returns a random email address.
+// Address function returns a random male email address.
 // A convenience function, same as g.GenericGenerator("email_male"),
 // but only for en_US language
 // Sample Output: Jay.Hayden@fakemail.com
@@ -16,7 +16,7 @@ func (g *Generator) EmailMale() string {
 	return g.GenericGenerator("email_male")
 }
 
-// Address function returns a random email address.
+// Address function returns a random female email address.
 // A convenience function, same as g.GenericGenerator("email_female"),
 // but only for en_US language
 // Sample Output: Alice.Hayden@fakemail.com
