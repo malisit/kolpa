36d81451ff228e675c26966452bd5280600206b1
Dmitrij Ivanenko <ivanenko.dmitrij@gmail.com>
2017-07-29 21:07:48 +0300

    email generation function improvement

--- email.go
+++ email.go
@@ -5,8 +5,21 @@ package kolpa
 // but only for en_US language
 // Sample Output: Jay.Hayden@fakemail.com
 func (g *Generator) Email() string {
-	if g.Locale_ != "en_US" {
-		g.Locale_ = "en_US"
-	}
 	return g.GenericGenerator("email")
+}
+
+// Address function returns a random email address.
+// A convenience function, same as g.GenericGenerator("email_male"),
+// but only for en_US language
+// Sample Output: Jay.Hayden@fakemail.com
+func (g *Generator) EmailMale() string {
+	return g.GenericGenerator("email_male")
+}
+
+// Address function returns a random email address.
+// A convenience function, same as g.GenericGenerator("email_female"),
+// but only for en_US language
+// Sample Output: Alice.Hayden@fakemail.com
+func (g *Generator) EmailFemale() string {
+	return g.GenericGenerator("email_female")
 }
